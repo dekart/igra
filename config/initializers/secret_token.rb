1cd94ea11f6f32576d955fa16aef28b59b56c65d
DEkart <rene.dekart@gmail.com>
2013-08-23 12:34:26 +0600

    Initial Rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Igra::Application.config.secret_key_base = '3d0674ec8961de3cea4b610055573cde8481adec088df87e9f32445966b58e08d8869e512384cae4cd7c188a64b223633c9e96a1123c42518211b6e88167b2c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Igra::Application.config.session_store :cookie_store, key: '_igra_session'
